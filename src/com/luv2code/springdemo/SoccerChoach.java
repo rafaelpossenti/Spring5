7a2db3cbb56838fcf57de50ee0dc8cd13abfd1e0
Rafael Possenti <rafael_possenti@hotmail.com>
2018-02-14 21:38:58 -0200

    First Commit

---
+++ src/com/luv2code/springdemo/SoccerChoach.java
@@ -0,0 +1,8 @@
+package com.luv2code.springdemo;
+
+public class SoccerChoach implements Coach{
+	@Override
+	public String getDailyWorkout() {
+		return "play soccer everyday at nine O'clock";
+	}
+}
