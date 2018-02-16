c1a539b4bb2873c857e58b2a23a5184f1c2a317b
Rafael Possenti <rafael_possenti@hotmail.com>
2018-02-16 00:43:42 -0200

    set Inject on xml context

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -2,13 +2,26 @@ package com.luv2code.springdemo;
 
 public class CricketCoach implements Coach{
 
+	private FortuneService fortuneService;
+	
+	public CricketCoach() {
+		System.out.println("inside no-arg");
+	}
+
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("inside set method");
+		this.fortuneService = fortuneService;
+	}
+
+
+
 	@Override
 	public String getDailyWorkout() {
-		return null;
+		return "Practice fast bowling for 15 minutes a day";
 	}
 	
 	@Override
 	public String getDailyFortune() {
-		return null;
+		return fortuneService.getFortune();
 	}
 }
