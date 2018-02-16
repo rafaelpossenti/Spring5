00e876499f12934ec22f4f562b886d67a9046a00
Rafael Possenti <rafael_possenti@hotmail.com>
2018-02-15 23:21:51 -0200

    Setter injection

---
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -0,0 +1,14 @@
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		return null;
+	}
+	
+	@Override
+	public String getDailyFortune() {
+		return null;
+	}
+}
