461491806ae47ad7faece5acc0e969d005fd8d21
Rafael Possenti <rafael_possenti@hotmail.com>
2018-02-17 00:07:03 -0200

    random getFortune()

--- src/com/luv2code/springdemo/HappyFortuneService.java
+++ src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,10 +1,17 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
 
+	private int[] fortune = new int[]{3,4,6}; 
+	
 	@Override
 	public String getFortune() {
-		return "today is your  lucky day!";
+		Random random = new Random();
+		int nAleatorio = random.nextInt(fortune.length); 
+		int value = fortune[nAleatorio];
+		return "today is your  lucky day! " + value;
 	}
 
 }
