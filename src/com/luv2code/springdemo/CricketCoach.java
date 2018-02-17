93d09c1f139ff9a69d83fd0a63ae8b9707afa666
Rafael Possenti <rafael_possenti@hotmail.com>
2018-02-16 23:02:47 -0200

    properties file injection

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -4,16 +4,37 @@ public class CricketCoach implements Coach{
 
 	private FortuneService fortuneService;
 	
+	private String emailAddress;
+	private String team;
+	
 	public CricketCoach() {
 		System.out.println("inside no-arg");
 	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach inside set email");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach inside set team");
+		this.team = team;
+	}
+
+
 
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("inside set method");
 		this.fortuneService = fortuneService;
 	}
-
-
 
 	@Override
 	public String getDailyWorkout() {
